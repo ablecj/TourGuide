16dcd2201d57a0ee2df5f462228282c81b5dcf0a
Able c j <gracyebin7@gmail.com>
2025-04-17 16:53:38 +0530

    Tour guide site created

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Banner = () => {
+  return (
+    <Carousel>
+    <Carousel.Item interval={1000}>
+      <img
+        className="d-block w-100"
+        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtnvAOajH9gS4C30cRF7rD_voaTAKly2Ntaw&s'
+        alt="First slide"
+      />
+      <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+
+    <Carousel.Item interval={1000}>
+      <img
+        className="d-block w-100"
+        src="https://media.istockphoto.com/id/1403500817/photo/the-craggies-in-the-blue-ridge-mountains.jpg?s=612x612&w=0&k=20&c=N-pGA8OClRVDzRfj_9AqANnOaDS3devZWwrQNwZuDSk="
+        alt="Second slide"
+      />
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+
+    <Carousel.Item interval={1000}>
+      <img
+        className="d-block w-100"
+        src="https://media.istockphoto.com/id/1403500817/photo/the-craggies-in-the-blue-ridge-mountains.jpg?s=612x612&w=0&k=20&c=N-pGA8OClRVDzRfj_9AqANnOaDS3devZWwrQNwZuDSk="
+        alt="Third slide"
+      />
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
+  );
+};
+
+export default Banner;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "../Styles/Navbar.css";
+import { Navbar, Container, Nav, NavDropdown, Offcanvas, Form, FormControl, Button } from 'react-bootstrap';
+
+
+const Header = () => {
+  return (
+    <>
+    {['lg'].map((expand) => (
+      <Navbar key={expand} expand={expand} sticky="top" className="bg-body-tertiary p-4">
+        <Container fluid>
+          <Navbar.Brand href="/">
+            <img
+              // src={logo}
+              alt="Logo"
+              height="30"
+              className="d-inline-block align-top"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title className="brand_name" id={`offcanvasNavbarLabel-expand-${expand}`}>
+                Brand Name
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="ms-auto pe-3">
+                <Nav.Link href="/" className="Nav_link">Home</Nav.Link>
+                <Nav.Link href="#About" className="Nav_link">About</Nav.Link>
+                <Nav.Link href="#Services" className="Nav_link">Services</Nav.Link>
+                <Nav.Link href="#Contact" className="Nav_link">Contact Us</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    ))}
+  </>
+  );
+};
+
+export default Header;
